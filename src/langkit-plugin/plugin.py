97033a75435ac355d5fba5888045b556dcc1e88e
Damien De Campos <damiensporting@gmail.com>
2022-06-07 15:31:31 +0200

    Rascal types in a OO style (1st version)

--- src/langkit-plugin/plugin.py
+++ src/langkit-plugin/plugin.py
@@ -9,12 +9,14 @@ class PluginPass(langkit.passes.AbstractPass):
         super().__init__("Plugin Pass!!!")
 
     def run(self, context: CompileCtx) -> None:
-        print("----- Plugin -----")
-        # self.emit_dot_visualisation(context)
+        # self.emit_dot_visualization(context)
         self.emit_rascal_data_types(context)
+        # self.print_leaf_types(context)
+        # self.emit_only_one_child_data_types(context)
+        return
 
     @staticmethod
-    def emit_dot_visualisation(context: CompileCtx) -> None:
+    def emit_dot_visualization(context: CompileCtx) -> None:
         print("digraph D {")
         for n in context.entity_types:
             if n.base is not None:
@@ -23,19 +25,68 @@ class PluginPass(langkit.passes.AbstractPass):
 
     @staticmethod
     def emit_rascal_data_types(context: CompileCtx) -> None:
+        rascal_types = {}
         for n in context.entity_types:
-            print(f"data {n.api_name} = (", end="")
-            if n.element_type.is_root_list_type:
+            if n.is_root_type:
+                # Handle Ada_Node root type separately
+                rascal_types[n.api_name] = [f"data {n.api_name}(loc src=|unknown:///|) = {n.api_name.lower}("]
+            else:
+                base = n.base.api_name
+                if base in rascal_types:
+                    # Adding constructor in parent type
+                    rascal_types[base].append(f"\n| {n.api_name.lower}({n.api_name} {n.api_name.lower})")
+
+                    rascal_types[n.api_name] = [f"data {n.api_name}(loc src=|unknown:///|) = {n.api_name.lower}("]
+                else:
+                    print(f"{base} not in rascal_types when processing {n.api_name}")
+                    continue
+
+            if n.element_type.is_list_type:
                 # Adding a field "content" for list node
-                print(f"List[{n.element_type.element_type.api_name}] content", end="")
+                rascal_types[n.api_name].append(f"list[{n.element_type.element_type.entity.api_name}] content")
             else:
                 fields = n.element_type.get_parse_fields(include_inherited=True)
                 i = 0
                 for field in fields:
                     field_type_name = field.type.entity.api_name if field.type.is_ast_node else field.type.api_name
-                    print(f"{field_type_name} {field.api_name}", end="")
+                    rascal_types[n.api_name].append(f"{field_type_name} {field.api_name}")
                     if i < len(fields) - 1:
-                        print(", ", end="")
+                        rascal_types[n.api_name].append(", ")
                     i = i + 1
-            print(")")
+            rascal_types[n.api_name].append(")")
 
+        print("""@license{
+            Copyright (c) 2022, TNO (ESI) and NWO-I Centrum Wiskunde & Informatica (CWI)
+            All rights reserved.
+            
+            Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+            
+            1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+            
+            2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+            
+            THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+            }
+            @author{Jurgen J. Vinju - Centrum Wiskunde & Informatica}
+            @author{Damien De Campos - TNO ESI}
+            @author{Pierre van de Laar - TNO ESI}
+            module lang::ada::AST
+            
+            import IO;
+            import List;\n\n""")
+        for value in rascal_types.values():
+            print(''.join(value) + ";\n")
+
+    @staticmethod
+    def emit_leaf_types(context: CompileCtx) -> None:
+        print("subtype Ada_Leaf_Node is LALCO.Ada_Node_Kind_Type \nwith Static_Predicate => Ada_Leaf_Node in ")
+        for cls in context.astnode_types:
+            if not cls.abstract and len(cls.derivations) == 0:
+                print(f"LALCO.{cls.ada_kind_name} ")
+                print("| ", end="")
+
+    @staticmethod
+    def emit_only_one_child_data_types(context: CompileCtx) -> None:
+        for n in context.entity_types:
+            if len(n.element_type.get_parse_fields(include_inherited=True)) == 1:
+                print(f"{n.api_name}")
