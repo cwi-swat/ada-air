01c2eff028ecd2fc3439cb70e0cfdee6d956b14b
Damien De Campos <damiensporting@gmail.com>
2022-06-10 11:21:34 +0200

    Simplifying rascal types generated
    Only nodes that derive from Ada_Node are Rascal data node.
    Regrouping LAL nodes into more general Rascal nodes (e.g. Def, Decl,
    Stmt, Expr...).
    Replacing Boolean by "Maybe".
    Adding Ada_Node alias.

--- src/langkit-plugin/plugin.py
+++ src/langkit-plugin/plugin.py
@@ -1,15 +1,16 @@
 import langkit.passes
 from langkit.compile_context import CompileCtx
-from langkit.compiled_types import SymbolType, CompiledType
+from langkit.compiled_types import SymbolType, CompiledType, EntityType
 
 
-def is_boolean_type(t: CompiledType):
+def is_boolean_node(t: CompiledType):
     derived_type = t.derivations
-    return ((len(derived_type) == 2 and all(d.api_name.lower.lower().endswith("_absent") or
-                                          d.api_name.lower.lower().endswith("_present")
-                                          for d in derived_type))
-    or (t.api_name.lower.lower().endswith("_absent")
-    or t.api_name.lower.lower().endswith("_present")))
+    return len(derived_type) == 2 and all(is_absent_or_present_node(d) for d in derived_type)
+
+
+def is_absent_or_present_node(t: CompiledType):
+    return (t.api_name.lower.lower().endswith("_absent")
+    or t.api_name.lower.lower().endswith("_present"))
 
 
 class PluginPass(langkit.passes.AbstractPass):
@@ -20,64 +21,115 @@ class PluginPass(langkit.passes.AbstractPass):
     def run(self, context: CompileCtx) -> None:
         # self.emit_dot_visualization(context)
         self.emit_rascal_data_types(context)
-        # self.print_leaf_types(context)
-        # self.emit_only_one_child_data_types(context)
         return
 
     @staticmethod
     def emit_dot_visualization(context: CompileCtx) -> None:
+        ref = PluginPass.compute_nb_references(context)
         print("digraph D {")
         for n in context.entity_types:
             if n.base is not None:
-                print(f"{n.base.api_name} -> {n.api_name}")
+                child = n.api_name if n.api_name.lower.lower() not in ref else f"{n.api_name} {ref[n.api_name.lower.lower()]}"
+                parent = n.base.api_name if n.base.api_name.lower.lower() not in ref else f"{n.base.api_name} {ref[n.base.api_name.lower.lower()]}"
+
+                print("\"{0}\" -> \"{1}\"".format(parent, child))
         print("}")
+
+    @staticmethod
+    def get_associated_rascal_type(t: EntityType) -> str:
+        field_type_name = None
+        inheritance_chain = t.element_type.get_inheritance_chain()
+        if t.is_root_type:
+            field_type_name = None
+        elif any(node.api_name.lower.endswith("_decl") for node in inheritance_chain):
+            field_type_name = "Decl"
+        elif any(node.api_name.lower.endswith("_def") for node in inheritance_chain):
+            field_type_name = "Def"
+        elif any("_expr" in node.api_name.lower for node in inheritance_chain):
+            field_type_name = "Expr"
+        elif any("stmt" in node.api_name.lower for node in inheritance_chain):
+            field_type_name = "Stmt"
+        elif any("assoc" in node.api_name.lower for node in inheritance_chain):
+            field_type_name = "Assoc"
+        elif any(node.api_name.lower.endswith("_node") and not "ada_node" in node.api_name.lower for node in
+                 inheritance_chain):
+            field_type_name = "Keyword"
+        elif any(node.api_name.lower.endswith("_spec") for node in inheritance_chain):
+            field_type_name = "Spec"
+        else:
+            field_type_name = inheritance_chain[1].public_type.api_name.camel_with_underscores
+        return field_type_name
+
+    @staticmethod
+    def get_rascal_field_type_name(field_type: EntityType) -> str:
+        field_type_name = None
+        inheritance_chain = field_type.element_type.get_inheritance_chain()
+        if field_type.is_root_type:
+            field_type_name = "Ada_Node"
+        elif any(node.api_name.lower.endswith("_decl") for node in inheritance_chain):
+            field_type_name = "Decl"
+        elif any(node.api_name.lower.endswith("_def") for node in inheritance_chain):
+            field_type_name = "Def"
+        elif any("_expr" in node.api_name.lower for node in inheritance_chain):
+            field_type_name = "Expr"
+        elif any("stmt" in node.api_name.lower for node in inheritance_chain):
+            field_type_name = "Stmt"
+        elif any("assoc" in node.api_name.lower for node in inheritance_chain):
+            field_type_name = "Assoc"
+        elif any(node.api_name.lower.endswith("_node") and not "ada_node" in node.api_name.lower for node in inheritance_chain):
+            field_type_name = "Keyword"
+        elif any(node.api_name.lower.endswith("_spec") for node in inheritance_chain):
+            field_type_name = "Spec"
+        elif field_type.element_type.is_list_type:
+            element_contained = field_type.element_type.element_type.entity
+            field_type_name = f"list[{PluginPass.get_rascal_field_type_name(element_contained)}]"
+        elif is_boolean_node(field_type.element_type):
+            field_type_name = f"Maybe[{inheritance_chain[1].public_type.api_name.camel_with_underscores}]"
+        else:
+            field_type_name = inheritance_chain[1].public_type.api_name.camel_with_underscores
+        return field_type_name
 
     @staticmethod
     def emit_rascal_data_types(context: CompileCtx) -> None:
         rascal_types = {}
         for n in context.entity_types:
             if n.is_root_type:
-                # skipping Ada_Node root type, we will use rascal node
+                # skipping Ada_Node, we will use rascal node
                 continue
-            elif n.element_type.is_list_type or n.api_name.lower.lower() == "ada_list":
-                # skipping list types, we will use rascal list
+            elif n.element_type.is_list_type or n.api_name.lower == "ada_list":
+                # skipping List nodes, we will use rascal list
                 continue
-            elif is_boolean_type(n.element_type):
-                # skipping present and absent, we will use boolean
+            elif is_absent_or_present_node(n.element_type):
+                # skipping Present and Absent nodes, we will use their base node with "Maybe"
                 continue
             else:
-                base = n.base.api_name
-                if base in rascal_types:
-                    # Adding constructor in parent type
-                    rascal_types[base].append(f"\n| {n.api_name.lower}({n.api_name} {n.api_name.lower})")
+                rascal_name = PluginPass.get_associated_rascal_type(n)
+                if rascal_name not in rascal_types:
+                    if n.astnode.abstract and not is_boolean_node(n.element_type):
+                        rascal_types[rascal_name] = [f"data {rascal_name}(loc src=|unknown:///|) ="]
+                        continue
+                    else:
+                        rascal_types[rascal_name] = [f"data {rascal_name}(loc src=|unknown:///|) = {n.api_name.lower} ("]
 
-                    rascal_types[n.api_name] = [f"data {n.api_name}(loc src=|unknown:///|) = {n.api_name.lower}("]
-                elif n.base.is_root_type:
-                    rascal_types[n.api_name] = [f"data {n.api_name}(loc src=|unknown:///|) = {n.api_name.lower}("]
                 else:
-                    print(f"{base} not in rascal_types when processing {n.api_name}")
-                    continue
+                    if n.astnode.abstract:
+                        continue
+                    # TODO find a better way to check if there is at least one constructor
+                    elif not rascal_types[rascal_name][-1].endswith("="):
+                        rascal_types[rascal_name].append("\n|")
+                    rascal_types[rascal_name].append(f" {n.api_name.lower} (")
 
                 fields = n.element_type.get_parse_fields(include_inherited=True)
                 i = 0
                 for field in fields:
                     assert field.type.is_ast_node
                     field_type = field.type.entity
-                    field_type_name = field.type.entity.api_name
-                    if field_type.is_root_type:
-                        field_type_name = "node"
-                    elif field_type.element_type.is_list_type:
-                        element_contained = field_type.element_type.element_type.entity
-                        element_contained_type = "node" if element_contained.is_root_type else element_contained.api_name
-                        field_type_name = f"list[{element_contained_type}]"
-                    elif is_boolean_type(field_type.element_type):
-                        field_type_name = "bool"
-
-                    rascal_types[n.api_name].append(f"{field_type_name} {field.api_name}")
+                    field_type_name = PluginPass.get_rascal_field_type_name (field_type)
+                    rascal_types[rascal_name].append(f"{field_type_name} {field.api_name}")
                     if i < len(fields) - 1:
-                        rascal_types[n.api_name].append(", ")
+                        rascal_types[rascal_name].append(", ")
                     i = i + 1
-                rascal_types[n.api_name].append(")")
+                rascal_types[rascal_name].append(")")
 
         # TODO use mako templates
         print("""@license{
@@ -98,20 +150,9 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 module lang::ada::AST
 
 import IO;
-import List;\n\n""")
+import List;
+import util::Maybe;
+alias Ada_Node = node;\n\n""")
+
         for value in rascal_types.values():
             print(''.join(value) + ";\n")
-
-    @staticmethod
-    def emit_leaf_types(context: CompileCtx) -> None:
-        print("subtype Ada_Leaf_Node is LALCO.Ada_Node_Kind_Type \nwith Static_Predicate => Ada_Leaf_Node in ")
-        for cls in context.astnode_types:
-            if not cls.abstract and len(cls.derivations) == 0:
-                print(f"LALCO.{cls.ada_kind_name} ")
-                print("| ", end="")
-
-    @staticmethod
-    def emit_only_one_child_data_types(context: CompileCtx) -> None:
-        for n in context.entity_types:
-            if len(n.element_type.get_parse_fields(include_inherited=True)) == 1:
-                print(f"{n.api_name}")
