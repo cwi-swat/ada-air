a5aee45c66c0b1a86fd85cf21b0d457539d783d7
Damien De Campos <damiensporting@gmail.com>
2022-08-11 13:38:52 +0200

    check that JAVA_HOME is set

--- install_unix.sh
+++ install_unix.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ -z "${JAVA_HOME}" ]]; then
+  echo "Please set the JAVA_HOME environment variable"
+  exit
+fi
 pip install -U git+https://github.com/AdaCore/langkit.git@22.0
 PYTHONPATH=./src/langkit-plugin/ python ./Libadalang/manage.py generate --plugin-pass=rascal_plugin.RascalPass
 cd src/main/ada/; alr build; cd -
--- install_windows.ps1
+++ install_windows.ps1
@@ -1,3 +1,7 @@
+if ($null -eq $env:JAVA_HOME){
+	Write-Output "Please set the JAVA_HOME environment variable"
+	exit
+}
 pip install -U git+https://github.com/AdaCore/langkit.git@22.0
 $env:PYTHONPATH='.\src\langkit-plugin\' 
 python .\Libadalang\manage.py generate --plugin-pass=rascal_plugin.RascalPass
