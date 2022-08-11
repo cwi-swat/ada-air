9b4968b339f0ab43e4422658cbfb0193be624ad7
Damien De Campos <damiensporting@gmail.com>
2022-08-11 12:22:03 +0200

    build script & submodule

---
+++ install_unix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install -U git+https://github.com/AdaCore/langkit.git@22.0
+PYTHONPATH=./src/langkit-plugin/ python ./Libadalang/manage.py generate --plugin-pass=rascal_plugin.RascalPass
+cd src/main/ada/; alr build; cd -
+mvn compile
